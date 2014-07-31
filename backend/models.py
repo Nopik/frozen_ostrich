1c76d2aaf226b4efe1812bea10dff57b00a696a5
Kamil Burzynski <kamil@nopik.net>
2014-07-31 21:19:43 +0200

    Added Product model, registered apps

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  code = models.CharField( max_length=200, db_index=True, unique=True ) #unique product code
+  name = models.CharField( max_length=200 ) #human readable name
+  description = models.CharField( max_length=2000 )
+  inventory_count = models.IntegerField( default=0 )
+
