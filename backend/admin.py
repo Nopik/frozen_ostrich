8ede5e174c77ba02b5b7009c9bb33717c6a23924
Kamil Burzynski <kamil@nopik.net>
2014-07-31 21:51:27 +0200

    Making code more unreadable to adhere with python style guide

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from backend.models import Product
 
-class ProductAdmin( admin.ModelAdmin ):
+class ProductAdmin(admin.ModelAdmin):
 	list_display = ('code', 'name', 'inventory_count')
-	search_fields = [ 'code', 'name' ]
+	search_fields = ['code', 'name']
 
-admin.site.register( Product, ProductAdmin )
+admin.site.register(Product, ProductAdmin)
