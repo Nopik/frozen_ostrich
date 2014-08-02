54e6a8c9d790f98b125a0934dc748d302c97f90e
Kamil Burzynski <kamil@nopik.net>
2014-08-02 15:59:38 +0200

    Updated unit tests

--- ui/test/karma.conf.js
+++ ui/test/karma.conf.js
@@ -1,34 +1,49 @@
 module.exports = function(config){
 	config.set({
+    preprocessors: {
+      'src/js/**/*.coffee': [ 'coffee' ]
+    },
 
-		basePath : '../',
+    coffeePreprocessor: {
+      // options passed to the coffee compiler
+      options: {
+        bare: true,
+        sourceMap: false
+      },
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.coffee$/, '.js');
+      }
+    },
 
-		files : [
+    basePath: '../',
+
+		files: [
+			'resources/bower_components/underscore/underscore.js',
 			'resources/bower_components/angular/angular.js',
 			'resources/bower_components/angular-route/angular-route.js',
 			'resources/bower_components/angular-resource/angular-resource.js',
 			'resources/bower_components/angular-animate/angular-animate.js',
 			'resources/bower_components/angular-mocks/angular-mocks.js',
-			'src/js/**/*.js',
+			'src/js/**/*.coffee',
 			'test/unit/**/*.js'
 		],
 
-		autoWatch : true,
+		autoWatch: true,
 
 		frameworks: ['jasmine'],
 
-		browsers : ['Chrome'],
+		browsers: ['Chrome'],
 
-		plugins : [
-						'karma-chrome-launcher',
-						'karma-jasmine'
-						],
+		plugins: [
+      'karma-coffee-preprocessor',
+      'karma-chrome-launcher',
+      'karma-jasmine'
+		],
 
-		junitReporter : {
+		junitReporter: {
 			outputFile: 'test_out/unit.xml',
 			suite: 'unit'
 		}
-
 	});
 };
-
--- ui/test/unit/controllersSpec.js
+++ ui/test/unit/controllersSpec.js
@@ -6,16 +6,21 @@ describe('Frozen Ostrich App', function(){
 
 		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
 			$httpBackend = _$httpBackend_;
-			$httpBackend.expectGET('api/v1/product/').respond({objects:[{},{},{},{}]});
+			$httpBackend.expectGET('api/v1/product/').respond({objects:[
+        { code: '1' },
+        { code: '2' },
+        { code: '3' },
+        { code: '4' }
+      ]});
 
 			scope = $rootScope.$new();
 			ctrl = $controller('ProductListCtrl', {$scope: scope});
 		}));
 
 		it('should create "products" model with 4 products', inject(function($controller) {
-				expect(scope.products).toBeUndefined();
-				$httpBackend.flush();
-				expect(scope.products.length).toEqual( 4 );
+      expect(scope.products).toEqual( [] );
+      $httpBackend.flush();
+      expect(scope.products.length).toEqual( 4 );
 		}));
 	});
 
@@ -35,10 +40,10 @@ describe('Frozen Ostrich App', function(){
 		}));
 
 		it('should fetch "product" details', inject(function($controller) {
-				expect(scope.product).toBeUndefined();
-				$httpBackend.flush();
-				expect(scope.product.name).toEqual( dummyName );
+      expect(scope.product.isLoaded).toEqual( false );
+      $httpBackend.flush();
+      expect(scope.product.name).toEqual( dummyName );
+      expect(scope.product.isLoaded).toEqual( true );
 		}));
 	});
 });
-
